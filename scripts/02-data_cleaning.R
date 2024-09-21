c8f70ba63409e3c4d8fe09a15e037f07da8dd47c
JIEFENG LI <157642219+1223lli@users.noreply.github.com>
2024-09-21 17:29:49 -0400

    Add files via upload
    select the variable will be used

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -11,9 +11,14 @@ library(tidyverse)
 
 #### Clean data ####
 raw_data <- read_csv("data/raw_data/Car_theft_data.csv")
+selected_data <- raw_data %>%
+  select(
+    REPORT_DATE, OCC_DATE, REPORT_YEAR, REPORT_MONTH, REPORT_DAY, REPORT_DOY, REPORT_DOW, REPORT_HOUR, 
+    OCC_YEAR, OCC_MONTH, OCC_DAY, OCC_DOY, OCC_DOW, OCC_HOUR, 
+    DIVISION, LOCATION_TYPE, PREMISES_TYPE, HOOD_158
+  )
 
-clean_data <- na.omit(raw_data)
-glimpse(clean_data)
+clean_data <- na.omit(selected_data)
 
 
 
